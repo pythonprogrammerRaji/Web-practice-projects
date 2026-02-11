4c7a09503d81ff6c64cc6aba73f174a1c0dac77a
pythonprogrammerRaji <rajeshwarirajeshwari34879@gmail.com>
2026-02-11 10:34:59 +0530

    Add Music Player functionality

---
+++ Music Player/data.js
@@ -0,0 +1,39 @@
+const songs = [
+    {
+        name : "Kurchi Madathapetti",
+        path: 'https://youtu.be/8_2lQiW2KWA',
+        artist: "Thaman",
+        cover: 'https://i.ytimg.com/vi/mUr_1FNOR74/hqdefault.jpg'
+
+    },
+    
+    {
+        name : "DJ Tillu",
+        path: 'https://www.jiosaavn.com/song/tillu-anna-dj-pedithe/RScBVS0EUX4',
+        artist: "Ram Miriyala",
+        cover: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQlDGfSYWPFwAQpuvtQmjWDeFXxoKXwRvvmF3lcH9VBg&s'
+
+    },
+    {
+        name : "saranga Dhariya",
+        path: 'jiosaavn.com/song/saranga-dariya/KgJSWE1BaH8',
+        artist: "Magli",
+        cover: 'https://i.ytimg.com/vi/L0TJAYSVZX0/sddefault.jpg'
+
+    },
+    {
+        name : "Enkem Enkem kavale",
+        path: 'https://www.jiosaavn.com/song/inkem-inkem-inkem-kaavaale/GCEmUCJFe0k',
+        artist: "Sid sriram",
+        cover: 'https://c.saavncdn.com/237/Geetha-Govindam-Telugu-2018-20180921-500x500.jpg'
+
+    },
+    {
+        name : "Melaga",
+        path: 'https://jiosaavn.com/song/mellaga/XUUyRAJjBmc',
+        artist: "DSP",
+        cover: 'https://m.media-amazon.com/images/S/pv-target-images/63825a38e489142d5d3302cf46747783243136e943db134ac9c2c4fc7d9c4af6.jpg'
+
+    },
+    
+];
